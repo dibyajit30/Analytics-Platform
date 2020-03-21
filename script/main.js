7b78f2e5a8c02a7dd62d2e11f9ff46ffb801382e
dibyajit <dibyajit30@gmail.com>
2020-03-21 00:46:57 -0400

    Employee turnover dashboard embedded.

--- script/main.js
+++ script/main.js
@@ -8,6 +8,22 @@ app.controller('mainController', function($scope, $sce) {
   scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    
   vizElement.parentNode.insertBefore(scriptElement, vizElement);
 
+  var divElement = document.getElementById('viz1584764713356');
+  var vizElement = divElement.getElementsByTagName('object')[0];
+  if (divElement.offsetWidth > 800) {
+      vizElement.style.width = '1000px';
+      vizElement.style.height = '1027px';
+  } else if (divElement.offsetWidth > 500) {
+      vizElement.style.width = '1000px';
+      vizElement.style.height = '1027px';
+  } else {
+      vizElement.style.width = '100%';
+      vizElement.style.height = '2127px';
+  }
+  var scriptElement = document.createElement('script');
+  scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+  vizElement.parentNode.insertBefore(scriptElement, vizElement);
+
   $scope.showPurchaseAnalysis = function(){
       $scope.purchaseAnalysis = true;
       $scope.employeeTurnover = false;
